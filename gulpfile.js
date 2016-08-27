723e3db6acc35ddbfef7380b70dec1f0a659d7d8
YOYO19911111 <yueerwanwan1991@163.com>
2016-08-27 17:24:28 +0800

    homework2-master

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gls = require('gulp-live-server');
+var browserSync = require('browser-sync');
+
+const reload = browserSync.reload;
+
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch("src/*.scss", ['sass']);
+    gulp.watch("index.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/*.scss")
+        .pipe(sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("dist"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
